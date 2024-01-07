7e23f86ffa30b7209bb9dc2e81141495ffea2c93
atomiks <cc.glows@gmail.com>
2024-01-07 12:07:17 +1100

    chore: setup changesets and release

---
+++ .github/changeset-version.js
@@ -0,0 +1,9 @@
+const {execSync} = require('node:child_process');
+
+// This script is used by the `release.yml` workflow to update the version of the packages being released.
+// The standard step is only to run `changeset version` but this does not update the lock file.
+// So we also run `bun install`, which does this update.
+// This is a workaround until this is handled automatically by `changeset version`.
+// See https://github.com/changesets/changesets/issues/421.
+execSync('bunx changeset version');
+execSync('bun install --frozen-lockfile');
